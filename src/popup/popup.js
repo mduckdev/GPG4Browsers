51bfcc766c5ca5a3e0ee43069bdaf3b31adf72a8
mduckdev <mduckdev@gmail.com>
2024-05-06 11:53:30 +0200

    Tailwind autocompile and toggle button when encrypting added

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,5 +1,5 @@
 // popup.js
-document.addEventListener('DOMContentLoaded', async ()=> {
+document.addEventListener('DOMContentLoaded', async () => {
     // Odczytaj parametr GET zawierający zaznaczony tekst
     const params = new URLSearchParams(window.location.search);
     const selectedText = params.get('selectedText');
@@ -30,6 +30,36 @@ document.getElementById('encryptBtn').addEventListener('click', async () => {
 
 });
 document.getElementById('newKey').addEventListener('click', async () => {
-    browser.windows.create({ url: "addKey.html", width: 700, height: 400 })
+    browser.windows.create({ url: "../addKey.html", width: 700, height: 700 })
+    return;
+});
+
+document.getElementById('enterKeyToggle').addEventListener('click', async () => {
+    const keysLabel = document.getElementById("keysLabel");
+    const keysDropdown = document.getElementById("keysDropdown");
+    const newKey = document.getElementById("newKey");
+
+    const publicKeyLabel = document.getElementById("publicKeyLabel");
+    const publicKey = document.getElementById("publicKey");
+
+
+    const enterKeyToggle = document.getElementById("enterKeyToggle");
+
+    if (enterKeyToggle.checked) {
+        keysLabel.style.display = "none";
+        keysDropdown.style.display = "none";
+        newKey.style.display = "none";
+
+        publicKeyLabel.style.display = "block";
+        publicKey.style.display = "block";
+    } else {
+        keysLabel.style.display = "block";
+        keysDropdown.style.display = "block";
+        newKey.style.display = "block";
+
+        publicKeyLabel.style.display = "none";
+        publicKey.style.display = "none";
+    }
+
     return;
 });
