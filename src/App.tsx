de44d284453056f75250276fa6f7be9ef373de8f
mduckdev <mduckdev@gmail.com>
2024-07-24 10:43:53 +0200

    Added button to open in browser properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Main from './components/Main';
 import Navbar from './components/Navbar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUpRightAndDownLeftFromCenter } from '@fortawesome/free-solid-svg-icons';
+import Browser from 'webextension-polyfill';
 
 const App: React.FC = () => {
+
   const [activeTab, setActiveTab] = useState<string>('encryption');
+  const [isPopup, setIsPopup] = useState<boolean>(true);
+
+  useEffect(()=>{
+    const params = new URLSearchParams(window.location.search);
+    setIsPopup(params.get("popup")==="false"?false:true)
+  },[])
+
+  const openTab = ()=>{
+    Browser.tabs.create({ url: "popup.html?popup=false" });
+  }
+
   return (
   <div className="min-h-screen">
-    <div className="w-80">
+    <div className={`${isPopup?("w-80"):("w-full")} relative`}>
+    {
+      isPopup?(
+        <FontAwesomeIcon icon={faUpRightAndDownLeftFromCenter} className="absolute top-3 right-3 hover:cursor-pointer" onClick={openTab} />
+      ):(null)
+    }
       <Main activeTab={activeTab} setActiveTab={setActiveTab}/>
       <Navbar activeTab={activeTab} setActiveTab={setActiveTab}/>
     </div>
